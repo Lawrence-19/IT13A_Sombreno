eeec49c2b5a38036e53c5b77dd26a3591c93eb05
Lawrence <lawrencesombreno@gmail.com>
2025-02-13 18:04:49 +0800

    Added HelloWorld class

---
+++ Sombreno_IT13A_Activities/src/Prelim/Lab_act1_Arithmetics.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prelim;
+
+/**
+ *
+ * @author User
+ */
+    import java.util.Scanner;
+
+public class Lab_act1_Arithmetics {
+    public static void main(String[] args){
+    
+        Scanner s= new Scanner(System.in);
+        
+        System.out.print("Input first number:");
+        int x= s.nextInt();
+        
+        System.out.print("Input first number:");
+        int y= s.nextInt();
+        
+        System.out.print("Input first number:");
+        int z= s.nextInt();
+        
+        
+        int One= x*y+z;
+        int Two=(x-y)%z;
+        int Three=(x+y+z)/3;
+        int Four=x*z-(y*y);
+        
+        
+        System.out.println("10 * 4 + 6      =" + One);
+        System.out.println("(10 - 4) % 6    =" + Two);
+        System.out.println("(10 + 4 + 6)/ 3 =" + Three);
+        System.out.println("10 * 5 - (4-4)  =" + Four);
+    
+    }
+}
