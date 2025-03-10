22bd8bfd29b62bc71b0ee158f1669612d0cf1136
Lawrence <lawrencesombreno@gmail.com>
2025-03-11 07:56:47 +0800

    Added Palindrome

---
+++ Sombreno_IT13A_Activities/src/Prelim/Lab_Act3_palindrome.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prelim;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Lab_Act3_palindrome {
+    
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a word: ");
+        String word = in.nextLine();
+
+        String reversed = "";
+
+        for (int y = word.length() - 1; y >= 0; y--) {
+            reversed += word.charAt(y);
+        }
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println(word.toLowerCase() + " is a palindrome");
+        } else {
+            System.out.println(word.toLowerCase() + " is NOT a palindrome");
+        }
+    }   
+}
